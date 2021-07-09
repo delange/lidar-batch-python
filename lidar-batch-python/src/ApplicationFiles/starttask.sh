3b3add961eb5849dbd32759410bd35f8828a3b52
Remko <remko.de.lange@microsoft.com>
2021-07-09 16:37:51 +0200

    clean starttask.sh

--- lidar-batch-python/src/ApplicationFiles/starttask.sh
+++ lidar-batch-python/src/ApplicationFiles/starttask.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-##switch to home
-#cd ~
-##download miniconda Linux x64 installer
-#wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh -O ~/miniconda.sh
-#
-##run installation in silent mode
-#bash ~/miniconda.sh -b -p $HOME/miniconda
-#
-##environment initialization
-#source ~/miniconda/bin/activate
-#conda init
-#conda list
-#conda install -c conda-forge pdal
-#pdal --version
-#
-#---
+
 # openup permissions
 chmod 755 $AZ_BATCH_NODE_SHARED_DIR/task.sh
 chmod 755 $AZ_BATCH_NODE_SHARED_DIR/lidar_application.py
